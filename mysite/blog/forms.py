46f60772fcc724085b24408ae4cef58444f2b48a
YuuTeeGit <soro.2713@gmail.com>
2020-05-12 14:58:36 +0900

    Part3終了（Air)

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -3,7 +3,23 @@ from blog.models import Post,Comment
 
 class PostForm(forms.ModelForm):
 
-    class Meta():
+    class Meta():#forms.ModelFormクラスに機能を追加
         model = Post
         fields = ('author','title','text')
+
+        widgets = {#それぞれの入力項目の設定
+            'title':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'}),
+        }
+
+class CommentForm(forms.ModelForm):
+
+    class Meta():
+        model = Comment
+        fields = ('author','text')
+
+        widgets = {
+            'author':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'}),
+        }
         
